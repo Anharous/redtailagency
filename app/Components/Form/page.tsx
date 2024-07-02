7d08ee000bfc41343e7771b461c6481fd6058507
Amirtha Lakshmi <amirtha.0726@gmail.com>
2024-07-02 13:11:31 +0530

    feat: modified error function

--- app/Components/Form/page.tsx
+++ app/Components/Form/page.tsx
@@ -24,6 +24,8 @@ export default function page() {
   });
 
   const [errors, setErrors] = useState<Partial<FormData>>({});
+  const [touched, setTouched] = useState<Partial<{ [key in keyof FormData]: boolean }>>({});
+
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -32,6 +34,18 @@ export default function page() {
       [name]: value
     }));
   };
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    const { name } = e.target;
+    setTouched(prevState => ({
+      ...prevState,
+      [name]: true
+    }))
+    setErrors(prevState => ({
+      ...prevState,
+      [name]: formData[name as keyof FormData] ? '' : 'Required'
+    }));
+};
 
   const validate = () => {
     let formErrors: Partial<FormData> = {};
@@ -47,7 +61,6 @@ export default function page() {
 
     const formErrors = validate();
     if (Object.keys(formErrors).length === 0) {
-
       const response = await fetch('/api/send-email', {
         method: 'POST',
         headers: {
@@ -93,9 +106,10 @@ export default function page() {
                   <p>Contact us today to schedule a consultation with one of our experienced agents. We're here to answer your questions and provide personalized insurance solutions that meet your needs.</p>
                 </div>
                 <div className="namesub">
-                  <div className="name-container">
+                  <div className="name-contain">
                     <label htmlFor="fname">First name</label>
-                    <div className="name">
+                    <div className={`name-container ${errors.firstname && touched.firstname ? 'error-container' : ''} ${errors.lastname && touched.lastname ? 'error-container' : ''} ${errors.phone && touched.phone ? 'error-container' : ''} ${errors.phone && touched.phone ? 'error-container' : ''}`}>
+                    <div className="name" >
                       <Image src="form-user.svg" alt="user image" width={20} height={20} />
                       <div className="formline"></div>
                         <input
@@ -103,13 +117,17 @@ export default function page() {
                           name="firstname"
                           type="text"
                           value={formData.firstname}
-                          onChange={handleChange} />  
+                          onChange={handleChange} 
+                          onBlur={handleBlur}
+                          className={errors.firstname && touched.firstname ? 'error' : ''}/>  
+                    </div>
                     </div>
                     {errors.firstname && <span className="error">{errors.firstname}</span>}
+                    
                   </div>
-                  <div className="name-container">
+                  <div className="name-contain">
             <label htmlFor="fname">Last name</label>
-            <div className="name">
+            <div className={`name-container ${errors.firstname && touched.firstname ? 'error-container' : ''} ${errors.lastname && touched.lastname ? 'error-container' : ''} ${errors.phone && touched.phone ? 'error-container' : ''} ${errors.phone && touched.phone ? 'error-container' : ''}`}>            <div className="name">
               <Image src="form-user.svg" alt="user image" width={20} height={20} />
               <div className="formline"></div>
               <input
@@ -117,37 +135,46 @@ export default function page() {
                 name="lastname"
                 type="text"
                 value={formData.lastname}
-                onChange={handleChange}/>
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.lastname && touched.lastname ? 'error' : ''}/>
+            </div>
             </div>
             {errors.lastname && <span className="error">{errors.lastname}</span>}
 
           </div>
         </div>
         <div className="emailphone">
-          <div className="name-container">
+          <div className="name-contain">
             <label htmlFor="lname">E-name address</label>
-            <div className="name">
+            <div className={`name-container ${errors.firstname && touched.firstname ? 'error-container' : ''} ${errors.lastname && touched.lastname ? 'error-container' : ''} ${errors.phone && touched.phone ? 'error-container' : ''} ${errors.phone && touched.phone ? 'error-container' : ''}`}>            <div className="name">
               <Image src="form-mail.svg"alt="mail image" width={20} height={20} />
               <div className="formline"></div>
               <input
                 placeholder="E-mail address"
                 name="email"
                 value={formData.email}
-                onChange={handleChange}/>
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.email && touched.email ? 'error' : ''}/>
+            </div>
             </div>
             {errors.email && <span className="error">{errors.email}</span>}
 
           </div>
-          <div className="name-container">
+          <div className="name-contain">
             <label htmlFor="fname">Phone number</label>
-            <div className="name">
+            <div className={`name-container ${errors.firstname && touched.firstname ? 'error-container' : ''} ${errors.lastname && touched.lastname ? 'error-container' : ''} ${errors.phone && touched.phone ? 'error-container' : ''} ${errors.phone && touched.phone ? 'error-container' : ''}`}>            <div className="name">
               <Image src="form-phone.svg" alt="phone image" width={20} height={20} />
               <div className="formline"></div>
               <input
                 placeholder="phone number"
                 name="phone"
                 value={formData.phone}
-                onChange={handleChange}/>
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.phone && touched.phone ? 'error' : ''}/>
+            </div>
             </div>
             {errors.phone && <span className="error">{errors.phone}</span>}
 
@@ -156,12 +183,14 @@ export default function page() {
         <div className="message">
           <div className="messagebox">
             <label htmlFor="fname">Insurance coverage</label>
-            <input
+            <div className={`name-container ${errors.firstname && touched.firstname ? 'error-container' : ''} ${errors.lastname && touched.lastname ? 'error-container' : ''} ${errors.phone && touched.phone ? 'error-container' : ''} ${errors.phone && touched.phone ? 'error-container' : ''}`}>            <input
               // placeholder="Insurance coverage"
               name="insurance"
               value={formData.insurance}
               onChange={handleChange}
+              onBlur={handleBlur}
             />
+            </div>
           </div>
           {/* {errors.insurance && <span className="error">{errors.insurance}</span>} */}
 
@@ -169,18 +198,20 @@ export default function page() {
         <div className="message">
           <div className="messagebox">
             <label htmlFor="fname">How can we help?</label>
-            <input
+            <div className={`name-container ${errors.firstname && touched.firstname ? 'error-container' : ''} ${errors.lastname && touched.lastname ? 'error-container' : ''} ${errors.phone && touched.phone ? 'error-container' : ''} ${errors.phone && touched.phone ? 'error-container' : ''}`}>            <input
               id="message"
               //placeholder="Message"
               name="message"
               value={formData.message}
               onChange={handleChange}
+              onBlur={handleBlur}
             />
+            </div>
           </div>
           {/* {errors.message && <span className="error">{errors.message}</span>} */}
 
         </div>
-        <button type="submit">Get A Quote</button>
+        <button id="form-button" type="submit">Get A Quote</button>
       </form>
     </div> 
     <div className="right">
